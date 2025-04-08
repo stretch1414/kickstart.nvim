b20049464e6676cf8a2e57d2ee6553f20d0e5d8b
Joel Moser <joelmoser@Joels-MacBook-Pro.local>
2025-04-08 13:14:16 -0400

    Remove eslint_d from lint config

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -7,7 +7,7 @@ return {
       local lint = require 'lint'
       lint.linters_by_ft = {
         markdown = { 'markdownlint' },
-        javascript = { 'eslint', 'eslint_d' },
+        javascript = { 'eslint' },
       }
 
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
